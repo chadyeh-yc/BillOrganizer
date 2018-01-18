23b2938dbe0b3331879a187a13511b8312183bb4
葉永青 <jack770328@hotmail.com>
2018-01-18 16:13:39 +0800

    加上DisplayFormat貼近需求

--- BillOrganizer/Models/ViewModels/BillorganizerViewModel.cs
+++ BillOrganizer/Models/ViewModels/BillorganizerViewModel.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace BillOrganizer.Models.ViewModels
 {
     public class BillorganizerViewModel
     {
+        [DisplayFormat(DataFormatString = "{0:N0}")]
         public int Amount { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
         public DateTime Date { get; set; }
         public string Remark { get; set; }
         public BillType Type { get; set; }
