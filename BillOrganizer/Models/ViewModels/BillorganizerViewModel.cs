3d07e145455b96001e42de0fda83926960426040
葉永青 <jack770328@hotmail.com>
2018-01-18 15:42:46 +0800

    新增BillorganizerViewModel

---
+++ BillOrganizer/Models/ViewModels/BillorganizerViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BillOrganizer.Models.ViewModels
+{
+    public class BillorganizerViewModel
+    {
+        public int Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Remark { get; set; }
+        public BillType Type { get; set; }
+    }
+
+    public enum BillType
+    {
+        收入,
+        支出
+    }
+}
