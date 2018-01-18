7a337e6c0d99297b34a1580ca4d1f85a58c3c659
葉永青 <jack770328@hotmail.com>
2018-01-18 15:02:19 +0800

    加入 BillorganizerController

---
+++ BillOrganizer/Controllers/BillorganizerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BillOrganizer.Controllers
+{
+    public class BillorganizerController : Controller
+    {
+        // GET: Billorganizer
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
