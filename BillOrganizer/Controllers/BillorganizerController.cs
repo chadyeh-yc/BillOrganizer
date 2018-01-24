4f2914fd4e881a9c1f99eb06be3674389bdad388
葉永青 <jack770328@hotmail.com>
2018-01-25 02:15:07 +0800

    變更BillorganizerController的資料來源為JournalEntries Model

--- BillOrganizer/Controllers/BillorganizerController.cs
+++ BillOrganizer/Controllers/BillorganizerController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BillOrganizer.Models;
+using BillOrganizer.Models.ViewModels;
 
 namespace BillOrganizer.Controllers
 {
@@ -14,35 +16,19 @@ namespace BillOrganizer.Controllers
             return View();
         }
 
+        private readonly JournalEntries journalEntries = new JournalEntries();
+
         [ChildActionOnly]
         public ActionResult ShowHistory()
         {
-            var history = new List<Models.ViewModels.BillorganizerViewModel>();
-            Random rnd = new Random();
-            Models.ViewModels.BillType BillType;
-            for (int i = 0; i < 100; i++)
+            var result = journalEntries.AccountBook.Take(100).Select(c => new BillorganizerViewModel
             {
-                if (rnd.Next(2) == 0)
-                {
-                    BillType = Models.ViewModels.BillType.支出;
-                }
-                else
-                {
-                    BillType = Models.ViewModels.BillType.收入;
-                }
-                var BillorganizerItem = new Models.ViewModels.BillorganizerViewModel
-                {
-                    Type = BillType,
-                    Amount = rnd.Next(9999999),
-                    Date = new DateTime(2016, 1, 1).AddDays(rnd.Next(900)),
-                    Remark = @"Item" + i.ToString()
-                };
-                history.Add(BillorganizerItem);
-            }
-
-
-
-            return View(history);
+                 Type = c.Categoryyy == 1 ? BillType.支出 : BillType.收入,
+                 Amount = c.Amounttt,
+                 Date = c.Dateee,
+                 Remark = c.Remarkkk,
+            }).ToList();
+            return View(result);
         }
     }
 }
